47dc31aa08a45827202fadd69e8301f31ab9e2b1
admin <gkaeogus7@gmail.com>
2023-09-14 12:48:47 +0900

    230914

--- app.ts
+++ app.ts
@@ -1,11 +1,10 @@
-
-type Store = {
+interface Store {
   currentPage: number;
   feeds: NewsFeed[];
 }
 
-type News = {
-  id: number;
+interface News {
+  readonly id: number;
   time_ago: string;
   title: string;
   url: string;
@@ -13,29 +12,24 @@ type News = {
   content: string;
 }
 
-type NewsFeed = News & {
-  id: number;
+interface NewsFeed extends News {
   comments_count: number;
   url: string;
-  user: string;
-  time_ago: string;
   points: number;
-  title: string;
   read?: boolean;
 }
 
-type NewsDetail = News & {
+interface NewsDetail extends News {
   comments: NewsComment[];
 }
 
-type NewsComment = News & {
+interface NewsComment extends News {
   comments: NewsComment[];
   level: number;
 }
 
 const container: HTMLElement | null = document.getElementById('root');
 const ajax: XMLHttpRequest = new XMLHttpRequest();
-const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
 const store: Store = {
@@ -43,61 +37,68 @@ const store: Store = {
   feeds: [],
 };
 
-function getData(url: string): NewsFeed[] | NewsDetail {
-  ajax.open("GET", url, false); // 사이트에서 동기적으로 데이터를 가져오겠다는 옵션
-  ajax.send(); // 데이터를 가져온다.
-
-  return JSON.parse(ajax.response);
+function applyApiMixins(targetClass: any, baseClasses: any[]): void {
+  baseClasses.forEach(baseClass => {
+    Object.getOwnPropertyNames(baseClass.prototype).forEach(name => {
+      const descriptor = Object.getOwnPropertyDescriptor(baseClass.prototype, name);
+      
+      if (descriptor) {
+        Object.defineProperty(targetClass.prototype, name, descriptor);
+      }
+    });
+  });
 }
 
-function makeFeed(feeds) {
+class Api {
+  getRequest<AjaxResponse>(url: string): AjaxResponse {
+    const ajax = new XMLHttpRequest();
+    ajax.open('GET', url, false);
+    ajax.send();
+
+    return JSON.parse(ajax.response);
+  }
+}
+
+class NewsFeedApi {
+  getData(): NewsFeed[] {
+    return this.getRequest<NewsFeed[]>(NEWS_URL);
+  }
+}
+
+class NewsDetailApi {
+  getData(id: string): NewsDetail {
+    return this.getRequest<NewsDetail>(CONTENT_URL.replace('@id', id));
+  }
+}
+
+applyApiMixins(NewsFeedApi, [Api]);
+applyApiMixins(NewsDetailApi, [Api]);
+
+function makeFeeds(feeds: NewsFeed[]): NewsFeed[] {
   for(let i = 0; i < feeds.length; i++) {
-    i = 10;
-    i = 'abc';
     feeds[i].read = false;
   }
 
   return feeds;
 }
 
-function updateView(html){
+function updateView(html: string){
   if(container != null){
-    container.innerHTML = template;
+    container.innerHTML = html;
   } else {
     console.error('최상위 컨테이너가 없어 UI를 진행하지 못합니다.');
   }
 }
 function newsFeed(){
+  const api = new NewsFeedApi(NEWS_URL);
     // 자주 사용되는 패턴 배열형태로 저장을한다. 
   let newsFeed: NewsFeed[] = store.feeds;
   const newsList = [];
   let template = `
-    <div class="bg-gray-600 min-h-screen">
-    <div class="bg-white text-xl">
-      <div class="mx-auto px-4">
-        <div class="flex justify-between items-center py-6">
-          <div class="flex justify-start">
-            <h1 class="font-extrabold">Hacker News</h1>
-          </div>
-          <div class="items-center justify-end">
-            <a href="#/page/{{__prev_page__}}" class="text-gray-500">
-              Previous
-            </a>
-            <a href="#/page/{{__next_page__}}" class="text-gray-500 ml-4">
-              Next
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="p-4 text-2xl text-gray-700">
-      {{__news_feed__}}
-    </div>
-    </div>
   `;
 
   if (newsFeed.length === 0){
-    newsFeed = store.feeds = makeFeed(getData(NEWS_URL));
+    newsFeed = store.feeds = makeFeeds(api.getData());
   }
 
   for (let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
@@ -123,8 +124,8 @@ function newsFeed(){
   }
 
   template = template.replace('{{__news_feed__}}', newsList.join(''));
-  template = template.replace('{{__prev_page__}}', store.currentPage > 1 ? store.currentPage - 1 : 1);
-  template = template.replace('{{__next_page__}}', store.currentPage + 1);  
+  template = template.replace('{{__prev_page__}}', String(store.currentPage > 1 ? store.currentPage - 1 : 1));
+  template = template.replace('{{__next_page__}}', String(store.currentPage + 1));  
 
   updateView(template);
 }
@@ -132,11 +133,10 @@ function newsFeed(){
 //console.log(ajax.response); // 데이터를 가져오는걸 성공했다.
 // 자바스크립트에서 보이는것을 응답 값을 객체로 바꿔볼꺼다. 보기쉽게하기위해서 Json형식만 가능하다.
 
-function newsDetail() {
+function newsDetail(): void {
   const id = location.hash.substr(7);
-
-  const newsContent = getData(CONTENT_URL.replace('@id', id))
-  const title = document.createElement("h1");
+  const api = new NewsDetailApi(CONTENT_URL.replace('@id', id));
+  const newsContent = api.getData();
   let template = `
   <div class="bg-gray-600 min-h-screen pb-8">
   <div class="bg-white text-xl">
@@ -173,34 +173,33 @@ function newsDetail() {
     }
   }
 
-  function makeComment(comments, called = 0) {
-    const commentString = [];
-  
-    for(let i = 0; i < comments.length; i++){
-      commentString.push(`
-        <div style="padding-left: ${called * 40}px;" class="mt-4">
-          <div class="text-gray-400">
-            <i class="fa fa-sort-up mr-2"></i>
-            <strong>${comments[i].user}</strong> ${comments[i].time_ago}
-          </div>
-          <p class="text-gray-700">${comments[i].content}</p>
-        </div>
-      `);
-
-      if (comments[i].comments.length > 0){
-        commentString.push(makeComment(comments[i].comments, called + 1));
-      }
-    }
-
-    return commentString.join('');
-  }
-
   updateView(template.replace('{{__comments__}}', makeComment(newsContent.comments)));
 }
 
 
+function makeComment(comments, called = 0) {
+  const commentString = [];
 
-function router(){
+  for(let i = 0; i < comments.length; i++){
+    commentString.push(`
+      <div style="padding-left: ${called * 40}px;" class="mt-4">
+        <div class="text-gray-400">
+          <i class="fa fa-sort-up mr-2"></i>
+          <strong>${comments[i].user}</strong> ${comments[i].time_ago}
+        </div>
+        <p class="text-gray-700">${comments[i].content}</p>
+      </div>
+    `);
+
+    if (comments[i].comments.length > 0){
+      commentString.push(makeComment(comments[i].comments, called + 1));
+    }
+  }
+
+  return commentString.join('');
+}
+
+function router(): void{
   const routePath = location.hash;
 
   if(routePath === '') {
